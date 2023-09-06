5c1adad1ddfc12867298403360d3d6ae13036760
TarasMysiura <tarasmysiura@gmail.com>
2023-09-06 21:34:03 +0300

    init project

---
+++ server.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import app from "./app.js";
+
+
+
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+    console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  }) 
+
+
+
